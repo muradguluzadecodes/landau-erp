36397855ca9f442fa39cedf8bc994a7a200ae015
muradguluzadecodes <quluzade.m.98@gmail.com>
2025-11-28 14:35:36 +0400

    fix: change users page position

--- src/app/(users)/users/page.tsx
+++ src/app/(users)/users/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import UsersTable from '../../widgets/tables/usersTable/UsersTable';
+import UsersTable from '@/widgets/tables/usersTable/UsersTable';
 
 export default function Page() {
   return <UsersTable />;
